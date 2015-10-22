a2959db363cbfee1953f296e6e3abefae2690f00
Thomas Colomb <thomas.colomb@dailymotion.com>
2015-10-22 11:49:30 +0000

    add hhvm support

---
+++ php/ext_prefixdb.php
@@ -0,0 +1,8 @@
+<?hh
+
+ <<__NativeData("ZendCompat")>> class PrefixDB {
+  <<__Native("ZendCompat")>> public function __construct(string $path = null);
+  <<__Native("ZendCompat")>> public function add(string $prefix): mixed;
+  <<__Native("ZendCompat")>> public function save(string $path): inmixedt;
+  <<__Native("ZendCompat")>> public function search(string $address): mixed;
+}
